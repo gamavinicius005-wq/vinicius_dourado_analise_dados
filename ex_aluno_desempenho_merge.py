1f07710ca61993327390009650f66617aeb9e313
Vinicius Gama Dourado <gama.vinicius005@gmail.com>
2026-09-01 11:08:45 -0300

    att

---
+++ ex_aluno_desempenho_merge.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df_localidades = pd.read_csv("alunos_localidade_carro_merge.csv")
+df_alunos = pd.read_csv("alunos_desempenho_merge.csv")
--- teste.py
+++ teste.py
@@ -1,3 +1,11 @@
 print("Hello, World!")
 print ("hello2")
 
+import pandas as pd
+
+df = pd.DataFrame({
+    "Nome": ["João", "Maria", "Pedro"],
+    "Idade": [20, 22, 21]
+})
+
+print(df)
